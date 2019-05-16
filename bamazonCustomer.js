afcf5b59f32a754737e79c8ee297cd9d3861b13c
Ibrahim Iqbal <iaiqbal@uh.edu>
2019-05-16 16:22:39 -0500

    table display using loop

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,5 +27,8 @@ connection.connect(function(err) {
 function store() {
     connection.query("SELECT * FROM products", function(err, res) {
         if(err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
     });
 }
